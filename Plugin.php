8c0354a3261a57a2b5cef56d9318433e18ae0bf9
root <root@project-dev.pensoft.local>
2022-11-28 14:24:25 +0200

    aws

--- Plugin.php
+++ Plugin.php
@@ -1,8 +1,10 @@
 <?php namespace Pensoft\ServicesProvider;
 
 use Backend;
+use System\Classes\MarkupManager;
 use System\Classes\PluginBase;
 use Event;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * ServicesProvider Plugin Information File
@@ -50,12 +52,31 @@ class Plugin extends PluginBase
      */
     public function boot()
     {
-		Event::listen('rainlab.user.getNotificationVars', function ($user) {
-			$code = implode('!', [$user->id, $user->getActivationCode()]);
-			$link = url('/profile') . '?activate=' . $code . '&code=' . $code;
+        Event::listen('rainlab.user.getNotificationVars', function ($user) {
+            $code = implode('!', [$user->id, $user->getActivationCode()]);
+            $link = url('/profile') . '?activate=' . $code . '&code=' . $code;
 
-			return ['link' => $link, 'surname' => $user->surname];
-		});
+            return ['link' => $link, 'surname' => $user->surname];
+        });
+
+        MarkupManager::instance()->registerCallback(function ($manager) {
+            $manager->registerFunctions([
+                'getAWSFile' => function ($path) {
+                    $removeStr = 'https://s3-' . env('AWS_REGION') . '.amazonaws.com/' . env('AWS_BUCKET') . '/';
+                    $key = str_replace($removeStr, '', $path);
+                    $client = Storage::disk('s3')->getDriver()->getAdapter()->getClient();
+
+                    $command = $client->getCommand('GetObject', [
+                        'Bucket' => env('AWS_BUCKET'),
+                        'Key' => $key
+                    ]);
+                    $request = $client->createPresignedRequest($command, '+20 minutes');
+                    $presignedUrl = (string)$request->getUri();
+
+                    return $presignedUrl;
+                },
+            ]);
+        });
     }
 
     /**
